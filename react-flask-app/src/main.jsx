8284c7fe0d50de4a1cd4dd4240b7b277e5ac725b
p1aaku <211001042@rajalakshmi.edu.in>
2024-10-30 11:59:04 +0530

    Updated Frontend

--- react-flask-app/src/main.jsx
+++ react-flask-app/src/main.jsx
@@ -4,9 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import Details from './components/Details.jsx';
-import Dataset from './components/Dataset.jsx';
-import Questionnaire from './components/Questionnaire.jsx';
-import Result from './components/Result.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,9 +12,6 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/details" element={<Details />} />
-        <Route path="/dataset" element={<Dataset />} />
-        <Route path="/questionnaire" element={<Questionnaire />} />
-        <Route path="/result" element={<Result />} />
       </Routes>
     </Router>
   </StrictMode>,
